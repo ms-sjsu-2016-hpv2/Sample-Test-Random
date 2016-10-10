a9b54f0f90c5e6f3f377455fe6824854903c0659
pankaj_dighe <pankajdighe77@gmail.com>
2016-10-10 14:51:20 -0700

    Added the code for intel edison

---
+++ intel_edison/read_temperature_data.js
@@ -0,0 +1,22 @@
+var groveSensor = require('jsupm_grove');
+
+// Create the temperature sensor object using AIO pin 0
+var temp = new groveSensor.GroveTemp(1);
+console.log(temp.name());
+
+// Read the temperature ten times, printing both the Celsius and
+// equivalent Fahrenheit temperature, waiting one second between readings
+var i = 0;
+
+console.log("yo:"+temp.value());                                                                                                                                                                
+                                                                                                                                                                                                
+                                                                                                                                                                                                
+console.log(temp.value());                                                                                                                                                                      
+var waiting = setInterval(function() {                                                                                                                                                          
+        var celsius = temp.value();                                                                                                                                                             
+        var fahrenheit = celsius * 9.0/5.0 + 32.0;                                                                                                                                              
+        console.log(celsius + " degrees Celsius, or " +                                                                                                                                         
+            Math.round(fahrenheit) + " degrees Fahrenheit");                                                                                                                                    
+        i++;                                                                                                                                                                                    
+        if (i == 10) clearInterval(waiting);                                                                                                                                                    
+        }, 20000); 
